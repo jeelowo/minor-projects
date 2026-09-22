0e318394a730f2087ecff3d3e99eb12cb81a54cc
jeelowo <plegariajamuel47@gmail.com>
2026-09-22 16:50:25 +0800

    added StudentPortal
    AppDev Activity #2

--- StudentPortal/Program.cs
+++ StudentPortal/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace StudentPortal
 {
@@ -10,6 +6,28 @@ namespace StudentPortal
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter your full name: ");
+            String fullName = Console.ReadLine().Trim();
+            String upperFullName = fullName.ToUpper();
+            int nameLength = upperFullName.Length;
+            Console.Write("Enter your age: ");
+            int age = -1;
+            try
+            {
+                age = Convert.ToInt32(Console.ReadLine().Trim());
+                if (age > 0 && age < 150)
+                {
+                    Console.WriteLine($"\nPROFILE SUMMARY:\nNAME: {upperFullName}\nAGE: {age}\nNAME LENGTH: {nameLength}");
+                }
+                else
+                {
+                    Console.WriteLine("Invalid age. Try again.");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{e.Message} Pls try again.");
+            }
         }
     }
 }
